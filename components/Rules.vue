b47c5bc38247dffc4a6dcc0ff559e42f818657fd
mattheweis <61011279@kmitl.ac.th>
2020-12-06 15:52:40 +0700

    get all configurations

---
+++ components/Rules.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <h3>{{ext_ip}}</h3>
+        <h3>{{proto}}</h3>
+        <h3>{{eport}}</h3>
+        <h3>{{iport}}</h3>
+    </div>
+</template>
+
+<script>
+export default {
+    name:"Rules",
+    props:['ext_ip','proto','eport','iport']
+}
+</script>
+
+<style>
+
+</style>
--- pages/servers/_id/index.vue
+++ pages/servers/_id/index.vue
@@ -1,13 +1,70 @@
 <template>
-  
+  <v-container>
+    <v-row>
+      <pre
+        class="config pa-2"
+      ><b>Servername:</b>      {{serverConfiguration.name}}</pre>
+    </v-row>
+    <v-row>
+      <pre
+        class="config pa-2"
+      ><b>Uplink:</b>          {{serverConfiguration.uplink}}</pre>
+    </v-row>
+    <v-row>
+      <pre
+        class="config pa-2"
+      ><b>Status:</b>          {{serverConfiguration.status}}</pre>
+    </v-row>
+    <v-row>
+      <pre
+        class="config pa-2"
+      ><b>Location:</b>        {{serverConfiguration.location}}</pre>
+    </v-row>
+    <Rules
+      v-for="(index, i) in serverConfiguration.rules"
+      :key="i"
+      :ext_ip="index.ext_ip"
+      :proto="index.proto"
+      :eport="index.eport"
+      :iport="index.iport"
+    />
+  </v-container>
 </template>
 
 <script>
+import axios from 'axios'
+import Rules from '../../../components/Rules.vue'
 export default {
-
+  components: {
+    Rules,
+  },
+  data() {
+    return {
+      serverConfiguration: {},
+      url:
+        'https://92b68d42-d685-4146-abd7-1afd0113c712.mock.pstmn.io/api/v1/client/backends?id=',
+    }
+  },
+  async created() {
+    const config = {
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: 'qwertyuiop',
+      },
+    }
+    try {
+      const res = await axios.get(this.url + this.$route.params.id, config)
+      this.serverConfiguration = res.data
+      console.log(this.serverConfiguration)
+    } catch (error) {
+      console.log(error)
+    }
+  },
 }
 </script>
 
 <style>
-
+.config {
+  font-size: 1.3rem;
+}
 </style>
